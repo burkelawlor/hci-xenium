e11bf0450da8371bf4381ebddc8bc8a0415f8364
burkelawlor <burke.lawlor@gmail.com>
2026-06-08 17:02:12 -0600

    - update to include Australia samples - refactor to take 'File path' col from metadata

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -8,62 +8,26 @@ import re
 
 
 ### Metadata and paths
+metadata = pd.read_excel("./data/raw/metadata.xlsx", dtype=str).dropna(subset=["File path"])
 
-metadata = pd.read_excel("./data/raw/metadata.xlsx", dtype=str).dropna(subset=["GnomEx ID"])
 
-batch_1 = ['TC1', 'SCLE1', 'SCLE2', 'PNT1', 'PNT2', 'EPD1', 'EPD2', 'EPD3', 'EPD4', 'EPD5', 'EPD6', 'EPD7']
-batch_2 = ['NS1', 'NS2', 'NS3', 'NS4', 'SCLE3', 'SCLE4', 'SCLE5', 'EPD8', 'EPD9', 'EPD10', 'LPP1']
-batch_3 = ['LPP2', 'LPP3', 'LPP4', 'LP1', 'LP2', 'LP3', 'OLP1', 'OLP2', 'MAST1', 'MAST2', 'MAST3', 'OLP3', 'OLP4', 'OLP5', 'MAST6', 'OLP6']
-
+### Xenium paths
 data_root = Path("./data/raw/Xenium")
-batch_1_root = data_root / '26697R'
-batch_2_root = data_root / '20260401__191124__28129'
-batch_3_root = data_root / '20260506__195908__28200'
+xenium_paths = {row['Study ID']: data_root / row['File path'] for index, row in metadata.iterrows()}
 
-xenium_paths = {
-    "TC1":   batch_1_root / "output-XETG00516__0076657__Region_6__20251017__213834",
-    "SCLE1": batch_1_root / "output-XETG00516__0076678__Region_1__20251017__213834",
-    "SCLE2": batch_1_root / "output-XETG00516__0076678__Region_2__20251017__213834",
-    "PNT1":  batch_1_root / "output-XETG00516__0076678__Region_4__20251017__213834",
-    "PNT2":  batch_1_root / "output-XETG00516__0076678__Region_3__20251017__213834",
-    "EPD1":  batch_1_root / "output-XETG00516__0076657__Region_5__20251017__213834",
-    "EPD2":  batch_1_root / "output-XETG00516__0076657__Region_2__20251017__213834",
-    "EPD3":  batch_1_root / "output-XETG00516__0076678__Region_5__20251017__213834",
-    "EPD4":  batch_1_root / "output-XETG00516__0076678__Region_6__20251017__213834",
-    "EPD5":  batch_1_root / "output-XETG00516__0076657__Region_3__20251017__213834",
-    "EPD6":  batch_1_root / "output-XETG00516__0076657__Region_1__20251017__213833",
-    "EPD7":  batch_1_root / "output-XETG00516__0076657__Region_4__20251017__213834",
 
-    'NS1':   batch_2_root / 'output-XETG00516__0084912__Region_2__20260401__191409',
-    'NS2':   batch_2_root / 'output-XETG00516__0084920__Region_2__20260401__191409',
-    'NS3':   batch_2_root / 'output-XETG00516__0084912__Region_3__20260401__191409',
-    'NS4':   batch_2_root / 'output-XETG00516__0084912__Region_5__20260401__191409',
-    'SCLE3': batch_2_root / 'output-XETG00516__0084920__Region_3__20260401__191409',
-    'SCLE4': batch_2_root / 'output-XETG00516__0084912__Region_4__20260401__191409',
-    'SCLE5': batch_2_root / 'output-XETG00516__0084920__Region_1__20260401__191409',
-    'EPD8':  batch_2_root / 'output-XETG00516__0084912__Region_1__20260401__191409',
-    'EPD9':  batch_2_root / 'output-XETG00516__0084920__Region_5__20260401__191409',
-    'EPD10': batch_2_root / 'output-XETG00516__0084920__Region_4__20260401__191409',
-    'LPP1':  batch_2_root / 'output-XETG00516__0084912__Region_6__20260401__191409',
-
-    'LPP2':  batch_3_root / 'output-XETG00516__0076414__Region_7__20260506__200316',
-    'LPP3':  batch_3_root / 'output-XETG00516__0076660__Region_8__20260506__200317',
-    'LPP4':  batch_3_root / 'output-XETG00516__0076414__Region_3__20260506__200316',
-    'LP1':   batch_3_root / 'output-XETG00516__0076414__Region_4__20260506__200316',
-    'LP2':   batch_3_root / 'output-XETG00516__0076414__Region_4__20260506__200316',
-    'LP3':   batch_3_root / 'output-XETG00516__0076414__Region_5__20260506__200316',
-    'OLP1':  batch_3_root / 'output-XETG00516__0076660__Region_7__20260506__200317',
-    'OLP2':  batch_3_root / 'output-XETG00516__0076414__Region_6__20260506__200316',
-    'MAST1': batch_3_root / 'output-XETG00516__0076660__Region_5__20260506__200317',
-    'MAST2': batch_3_root / 'output-XETG00516__0076660__Region_3__20260506__200317',
-    'MAST3': batch_3_root / 'output-XETG00516__0076660__Region_4__20260506__200317',
-    'OLP3':  batch_3_root / 'output-XETG00516__0076660__Region_6__20260506__200317',
-    'OLP4':  batch_3_root / 'output-XETG00516__0076660__Region_1__20260506__200316',
-    'OLP5':  batch_3_root / 'output-XETG00516__0076414__Region_1__20260506__200316',
-    'MAST6': batch_3_root / 'output-XETG00516__0076414__Region_2__20260506__200316',
-    'OLP6':  batch_3_root / 'output-XETG00516__0076660__Region_2__20260506__200317',
+### Batch information
+batch_slides = {
+    'batch_1': ['0076657', '0076678'],
+    'batch_2': ['0084912', '0084920'],
+    'batch_3': ['0076414', '0076660'],
+    'australia': ['0029954', '0029955'],
 }
+slide_to_batch = {slide: batch for batch, slides in batch_slides.items() for slide in slides}
+studyid_to_batch = metadata.set_index('Study ID')['Slide'].map(slide_to_batch).to_dict()
 
+
+### Disease state information
 sample_sets = {
     'NL': ['NS1', 'NS2', 'NS3', 'NS4'],
     'RMC_EPD': ['EPD1', 'EPD2', 'EPD3', 'EPD4', 'EPD5', 'EPD6', 'EPD7'],
@@ -74,10 +38,11 @@ sample_sets = {
     'OLP': ['OLP1', 'OLP2', 'OLP3', 'OLP4', 'OLP5', 'OLP6'],
     'MAST': ['MAST1', 'MAST2', 'MAST3','MAST4', 'MAST5', 'MAST6'],
     'SCLE': ['SCLE1', 'SCLE2', 'SCLE3', 'SCLE4', 'SCLE5'],
-    'TC': ['TC1'],
+    'TC': ['TC1','TC1_2','TC2','TC3','TC4_TC8','TC5'],
+    'LM': ['LM1','LM2','LM3','LM4','LM6','LM7','LM8'],
 }
 
-### Functions for loading Xenium data
+
 
 def extract_XYcoords_for_cells(sdata):
     sdata["table"].obs["centroidX"] = sd.get_centroids(sdata['cell_circles'])["x"]
@@ -93,6 +58,6 @@ def load_adata_from_xenium(sample_name):
     adata.obs['cell_id_unique'] = sample_name + '_' + adata.obs['cell_id']
     adata.obs['sample_name'] = sample_name
     adata.obs['sample_set'] = next(k for k, v in sample_sets.items() if sample_name in v)
-    adata.obs['batch'] = 'batch_1' if sample_name in batch_1 else 'batch_2' if sample_name in batch_2 else 'batch_3' if sample_name in batch_3 else None
+    adata.obs['batch'] = studyid_to_batch[sample_name]
     
     return adata
